625a3aab5b24baa305f9f63ddd4a588a7e6c2440
hassan-1381 <hassanagabalazada81tr@gmail.com>
2023-12-10 21:23:29 +0330

    hooof

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -3,7 +3,17 @@ import React from "react";
 export default function Layout({ children }) {
   return (
     <>
-      <header>header</header>
+      <header className=" text-2xl bg-zinc-700 w-full  flex items-center  justify-around ">
+        <h1>
+          Book App
+        </h1>
+        <div>
+
+          <p>Botostart</p>
+          <p> | react js cource </p>
+        </div>
+
+      </header>
       {children}
       <footer>footer</footer>
     </>
